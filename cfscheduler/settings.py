cc6e5ac5d575e45b06241b013c66911b6e969ff7
serge <sergerivera@upcapes.org>
2022-12-17 16:19:58 +0800

    fixed settings

--- cfscheduler/settings.py
+++ cfscheduler/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-m=x_a5&!*qzqc^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['deployed-cfscheduler-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['deployed-cfscheduler-production.up.railway.app', 'https://deployed-cfscheduler-production.up.railway.app',  '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://deployed-cfscheduler-production.up.railway.app']
 # Application definition
