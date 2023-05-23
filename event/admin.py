854268bcd4c87fe58d864c0971b22cc914d0c331
serge <sergerivera@upcapes.org>
2023-05-23 21:40:14 +0800

    added google oauth in database

--- event/admin.py
+++ event/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Event, DateRange
+from .models import Event, DateRange, OauthCredentials
 
 admin.site.register(Event)
 admin.site.register(DateRange)
+admin.site.register(OauthCredentials)
