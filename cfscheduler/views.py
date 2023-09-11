fcd77175d2e5d3bdb3f72ae86507a7e4b29b5990
John Mark David Pintor <johnmarkdavid.pintor@gmail.com>
2023-09-11 22:04:41 +0800

    Change method used for Flow object creation

--- cfscheduler/views.py
+++ cfscheduler/views.py
@@ -15,7 +15,26 @@ from googleapiclient.discovery import build
 
 import google_auth_oauthlib.flow
 
-from cfscheduler.settings import BASE_URL
+from cfscheduler.settings import BASE_URL, DEBUG
+
+creds = OauthCredentials.objects.first()
+
+if DEBUG:
+	redirect_uri = "http://localhost"
+else:
+	redirect_uri = "https://deployed-cfscheduler-production.up.railway.app"
+
+client_config = {
+"web": {
+	"client_id": creds.client_id,
+	"client_secret": creds.client_secret,
+	"redirect_uris": [redirect_uri],
+	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+	"token_uri": "https://oauth2.googleapis.com/token",
+	"project_id": "completely-free-scheduler",
+	"auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs"
+	}
+}
 
 # permissions set for the Google App to be accessed currently it is set to view and download only
 # more permissions here https://developers.google.com/identity/protocols/oauth2/scopes
@@ -24,14 +43,14 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 # home view
 def generate_token(request, inviter_id):
 
-	# create flow from client credentials downloaded from Google cloud
-	flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file('creds.json', scopes=SCOPES)
+	# create flow from client credentials
+	flow = google_auth_oauthlib.flow.Flow.from_client_config(client_config, scopes=SCOPES)
 
 	# URI where the oauth2's response will be redirected this must match with one of the authorized URIs configured in Google Cloud
 	flow.redirect_uri = BASE_URL + 'oauthcallback/'
 
 	# configuring the authorization url which will be used to request from oauth2
-	authorization_url, state = flow.authorization_url(include_granted_scopes='true')
+	authorization_url, state = flow.authorization_url(include_granted_scopes='true', prompt='consent')
 
 	# store the state in the session
 	request.session['state'] = state
@@ -49,8 +68,7 @@ def callback(request):
 	state = request.session['state']
 
 	# create a new flow note that scopes were not defined because it gets appended to the previous scopes
-	flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-	 'creds.json', scopes=None, state=state)
+	flow = google_auth_oauthlib.flow.Flow.from_client_config(client_config, scopes=None, state=state)
 
 	# same redirect uri as before
 	flow.redirect_uri =  BASE_URL + 'oauthcallback/'
